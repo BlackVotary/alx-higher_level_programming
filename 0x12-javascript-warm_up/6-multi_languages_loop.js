5a5e8404ea8e1d885ebb8bccf7a9a9f275c3916e
Blackvotary <mhardelekey@yahoo.com>
2024-04-23 18:06:11 +0100

    java-sscript

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// prints data in an array with loop
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i;
+for (i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}
